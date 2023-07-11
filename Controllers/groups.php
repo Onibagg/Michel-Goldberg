262643e13f6d5572448d9514d89b939c5bc69d50
Gabin Demé <129855477+Onibagg@users.noreply.github.com>
2023-07-11 16:24:46 +0200

    page display

--- Controllers/groups.php
+++ Controllers/groups.php
@@ -1,5 +1,5 @@
 <?php
 include("Models/setup.html");
 include("Views/nav.html");
-include("Views/bio.html");
+include("Views/groups.html");
 ?>
---
+++ Groupes.php
@@ -0,0 +1,3 @@
+<?php
+include("Controllers/groups.php");
+?>
