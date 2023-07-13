a005a6c063fe0cbc18a6d8a441ec9704d18eaf02
Gabin Demé <129855477+Onibagg@users.noreply.github.com>
2023-07-13 11:53:38 +0200

    nav bar and achitechture work

--- Groupes/GOLDBERG(S).php
+++ Groupes/GOLDBERG(S).php
@@ -1,6 +1,6 @@
 <?php
 include("../Models/setup.html");
 include("../Views/nav.html");
-include("../Views/groups.html");
+include("../Views/goldbergs.html");
 include("../Views/bottom-bar.html");
 ?>
